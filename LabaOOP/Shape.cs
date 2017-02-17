c20757716bbd61d77de4843e84e57109f7075cbd
Nikita123007 <nikita.zabeyda@gmail.com>
2017-02-17 22:00:17 +0300

    Create IShape and Shape

---
+++ LabaOOP/Shape.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LabaOOP
+{
+    class Shape
+    {
+        protected PictureBox pictureBox;
+        protected int startX;
+        protected int startY;
+        protected int finishX;
+        protected int finishY;
+        protected Color color;
+
+        protected int StartX
+        {
+            get
+            {
+                return startX;
+            }
+
+            set
+            {
+                startX = value;
+            }
+        }
+        protected int StartY
+        {
+            get
+            {
+                return startY;
+            }
+
+            set
+            {
+                startY = value;
+            }
+        }
+        protected int FinishX
+        {
+            get
+            {
+                return finishX;
+            }
+
+            set
+            {
+                finishX = value;
+            }
+        }
+        protected int FinishY
+        {
+            get
+            {
+                return finishY;
+            }
+
+            set
+            {
+                finishY = value;
+            }
+        }
+        protected Color Color
+        {
+            get
+            {
+                return color;
+            }
+
+            set
+            {
+                color = value;
+            }
+        }
+
+        public Shape(int StartX, int StartY, int FinishX, int FinishY, Color Color, PictureBox pictureBox)
+        {
+            this.StartX = StartX;
+            this.StartY = StartY;
+            this.FinishX = FinishX;
+            this.FinishY = FinishY;
+            this.Color = Color;
+            this.pictureBox = pictureBox;
+        }
+    }
+}
